fe4b82714e4946efafd1bc5ffbd1afdd33155de0
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-03-17 06:25:30 -0700

    Task Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function start
+ *
+ * Return: output result
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
