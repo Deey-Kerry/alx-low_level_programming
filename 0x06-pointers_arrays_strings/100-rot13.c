88e80a82df4b23e85b106d2aae3e7a22d137ce15
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-03-29 12:04:14 -0700

    Task Rotate

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes a string using rot13
+ * @n: pointer
+ * Return: string
+ */
+char *rot13(char *n)
+{
+	int a;
+	int b;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (n[a] == data1[b])
+			{
+				n[a] = datarot[b];
+				break;
+			}
+		}
+	}
+	return (n);
+}
