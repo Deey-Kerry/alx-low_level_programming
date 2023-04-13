e721407857db510ffe8469c2ba05a8f718ecbf18
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-04-13 15:02:11 -0700

    Task multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,13 +18,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	x = 1;
 	while (x < argc)
 	{
 		y = 0;
 		while (argv[x][y] != '\0')
 		{
-			if (argv[x][y] > 9 || argv[x][y] < 0)
+			if (argv[x][y] > '9' || argv[x][y] < '0')
 			{
 				printf("Error\n");
 				exit(98);
