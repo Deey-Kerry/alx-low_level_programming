3fd73ed5ded5394de77a2759d1137c1481dc8c69
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-04-13 16:24:40 -0700

    Task multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * is_digit - checks for a non-digit char in string
+ * @s: string
+ * Return: 0 or 1
+ */
+int is_digit(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		if (s[a] < '0' || s[a] > '9')
+			return (0);
+		a++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - progran that returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	return (a);
+}
+
+/**
+ * errors - show errors
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - a program that multiplies two positive numbers
  * @argc: number of arguements
  * @argv: one dimensional array of arguements
@@ -10,36 +54,45 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long multiply;
-	int x, y;
+	char *s1, *s2;
+	int size1, size2, size, a, take, base1, base2, *multiply, b = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	size = size1 + size2 + 1;
+	multiply = malloc(sizeof(int) * size);
+	if (!multiply)
+		return (1);
+	for (a = 0; a <= size1 + size2; a++)
+		multiply[a] = 0;
+	for (size1 = size1 - 1; size1 >= 0; size1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (atoi(argv[1]) <= 0 || atoi(argv[2]) <= 0)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	x = 1;
-	while (x < argc)
-	{
-		y = 0;
-		while (argv[x][y] != '\0')
+		base1 = s1[size1] - '0';
+		take = 0;
+		for (size2 = _strlen(s2) - 1; size2 >= 0; size2--)
 		{
-			if (argv[x][y] > '9' || argv[x][y] < '0')
-			{
-				printf("Error\n");
-				exit(98);
-			}
-			y++;
+			base2 = s2[size2] - '0';
+			take += multiply[size1 + size2 + 1] + (base1 * base2);
+			multiply[size1 + size2 + 1] = take % 10;
+			take /= 10;
 		}
-		x++;
+		if (take > 0)
+			multiply[size1 + size2 + 1] += take;
 	}
+	for (a = 0; a < size - 1; a++)
+	{
+		if (multiply[a])
+			b = 1;
+		if (b)
+			_putchar(multiply[a] + '0');
+	}
+	if(!b)
+		_putchar('0');
 
-	multiply = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", multiply);
+	_putchar('\n');
+	free(multiply);
 	return (0);
 }
