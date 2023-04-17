08579285ffdc708539d44f09aca77c30f739309c
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-04-17 16:08:03 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,11 @@ struct dog
 	float age;
 };
 /**
- * dog_ck - dog struct
+ * dog_t - dog struct
  */
-typedef struct dog dog_ck;
-void init_dog(struct dog *g, char *name, float age, char *owner);
-void print_dog(struct dog *g);
-dog_ck *new_dog(char *name, float age, char *owner);
-void free_dog(dog_ck *g);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
-
