142552422c3f39a5175b9717288c4c405850bef9
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-04-19 16:01:08 -0700

    Update12

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,38 +10,38 @@
  */
 int main(int argc, char *argv[])
 {
-	char b;
-	int kd1, kd2, output;
-	int (*func)(int, int);
+        char b;
+        int kd1, kd2, output;
+        int (*func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (98);
-	}
+        if (argc != 4)
+        {
+                printf("Error\n");
+                return (98);
+        }
 
-	kd1 = atoi(argv[1]);
-	kd2 = atoi(argv[3]);
+        kd1 = atoi(argv[1]);
+        kd2 = atoi(argv[3]);
 
-	func = get_op_func(argv[2]);
+        func = get_op_func(argv[2]);
 
-	if (!func)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+        if (!func)
+        {
+                printf("Error\n");
+                exit(99);
+        }
 
-	b = *argv[2];
+        b = *argv[2];
 
-	if ((b == '/' || b == '%') && kd2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+        if ((b == '/' || b == '%') && kd2 == 0)
+        {
+                printf("Error\n");
+                exit(100);
+        }
 
-	output = func(kd1, kd2);
+        output = func(kd1, kd2);
 
-	printf("%d\n", output);
+        printf("%d\n", output);
 
-	return (0);
+        return (0);
 }
