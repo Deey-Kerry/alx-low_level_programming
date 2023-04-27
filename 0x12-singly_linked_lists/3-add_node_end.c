8a9ff3fe7f9afd46ab9d5570465f2f2558728db2
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-04-27 14:50:42 -0700

    a function that adds a new node at the end of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,41 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * add_node_end - a function that adds a new node at the end
- * of a list_t list
+ * add_node_end - a function that adds a new node at the end of
+ * a list_t list
  * @head: first node
- * @str: string to be duplicated
+ * @str: string
  * Return: new node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *last;
-	char *dup;
-	int len;
+	list_t *new;
+	list_t *last = *head;
+	unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-
-	if (!new)
-		return (NULL);
-
-	dup = strdup(str);
-	if (!dup)
-	{
-		free(new);
-		return (NULL);
-	}
 	for (len = 0; str[len];)
 		len++;
 
-	new->str = dup;
- 	new->len = len;
- 	new->next = NULL;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
-	}
+
+	while (last->next)
+		last = last->next;
+
+	last->next = new;
 
 	return (new);
 }
