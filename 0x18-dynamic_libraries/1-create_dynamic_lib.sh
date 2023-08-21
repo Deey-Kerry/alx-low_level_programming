553555be355b7e175171827389558b9dc3572639
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-21 15:16:18 -0700

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-
+gcc *.c -fpic -c
+gcc -shared *.o -o liball.so
