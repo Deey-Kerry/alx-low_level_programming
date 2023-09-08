5006c4e16f13d24f3af77aed5728e1d75eeeed9c
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-09-08 13:30:10 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,9 +21,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
+
 	for (a = 0; a < size; a++)
 		hash_table->array[a] = NULL;
 
