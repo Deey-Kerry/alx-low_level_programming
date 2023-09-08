03863e2945f3c1bd6e1fb7d491da4af8f1677c43
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-09-08 13:21:56 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int a;
 
 	hash_table = malloc(sizeof(hash_table_t));
+
 	if (hash_table == NULL)
 		return (NULL);
+
 	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
@@ -23,5 +26,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (a = 0; a < size; a++)
 		hash_table->array[a] = NULL;
+
 	return (hash_table);
 }
