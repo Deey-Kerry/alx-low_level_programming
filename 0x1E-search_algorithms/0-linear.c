7488677f1d8f34a4c677fb000614a170963c5a3e
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-12-13 14:33:52 -0800

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,6 @@
 #include "search_algos.h"
+#include <stdio.h>
+
 /**
  * linear_search - function that searches for a value in an array of
  * integers
@@ -7,6 +9,7 @@
  * @value:  is the value to search for
  * Return: value or NULL
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t a = 0;
