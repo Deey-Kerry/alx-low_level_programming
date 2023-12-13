1ce7a337114fa448ebc60ce5435b327eece668a3
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-12-13 05:39:27 -0800

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t a;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (a = 0; a < size; a++)
@@ -20,6 +20,5 @@ int linear_search(int *array, size_t size, int value)
 		if (array[a] == value)
 			return (a);
 	}
-
 	return (-1);
 }
